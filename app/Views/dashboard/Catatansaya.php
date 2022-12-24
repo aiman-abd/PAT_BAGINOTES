866400f773b02b9e41d5eb26cd9f9bf9cce55d21
ArdianXD <ardian25hilman@gmail.com>
2022-12-24 11:20:14 +0700

    modified:   app/Views/dashboard/Catatansaya.php

--- app/Views/dashboard/Catatansaya.php
+++ app/Views/dashboard/Catatansaya.php
@@ -6,19 +6,6 @@
         <div class="example">
             <ul class="nav nav-tabs" role="tablist">
                 <li class="nav-item" role="presentation"><a class="nav-link active" data-coreui-toggle="tab" href="#preview-512" role="tab" aria-selected="true">
-<<<<<<< HEAD
-                        <svg class="icon me-2">
-                            <use xlink:href="dist/vendors/@coreui/icons/svg/free.svg#cil-short-text"></use>
-                        </svg>Catatan</a></li>
-            </ul>
-            <div class="tab-content rounded-bottom">
-                <div class="tab-pane p-3 active preview" role="tabpanel" id="preview-512">
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th scope="col">Judul</th>
-                                <th scope="col">Subjek</th>
-=======
                     <svg class="icon me-2">
                         <use xlink:href="dist/vendors/@coreui/icons/svg/free.svg#cil-short-text"></use>
                     </svg>Catatan</a>
@@ -33,29 +20,11 @@
                                 <th scope="col">Judul</th>
                                 <th scope="col">Subjek</th>
                                 <th scope="col">Berkas</th>
->>>>>>> 70a51963c12c2180c212f4a20ebb2ece85661b46
                                 <th scope="col">Aksi</th>
                             </tr>
                         </thead>
                         <tbody>
                             <tr>
-<<<<<<< HEAD
-                                
-                                <td>Mark</td>
-                                <td>Otto</td>
-                                <td>@mdo</td>
-                            </tr>
-                            <tr>
-                                
-                                <td>Jacob</td>
-                                <td>Thornton</td>
-                                <td>@fat</td>
-                            </tr>
-                            <tr>
-                                
-                                <td colspan="2">Larry the Bird</td>
-                                <td>@twitter</td>
-=======
                                 <th scope="row">1</th>
                                 <td>Regressi Linier</td>
                                 <td>Probabilika dan Statistika</td>
@@ -72,7 +41,6 @@
                                     </svg>
                                     </button>
                                 </td>
->>>>>>> 70a51963c12c2180c212f4a20ebb2ece85661b46
                             </tr>
                         </tbody>
                     </table>
